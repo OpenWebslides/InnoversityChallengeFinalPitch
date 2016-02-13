aa1779f5d3bd64cea65a2106547b4d4a55cc68ab
Ruben Verborgh <ruben@verborgh.org>
2016-02-13 10:55:14 +0100

    Enable editing on click; remove on ESC.

--- scripts/edit.js
+++ scripts/edit.js
@@ -1,3 +1,12 @@
 $(function () {
-  $('#studenten').attr('contentEditable', true);
+  var slide = $('#studenten').on('mousedown', function () {
+    slide.attr('contentEditable', true)
+         .on('keydown', function disableHotkeys(event) {
+           event.stopPropagation();
+           if (event.keyCode === 27) {
+             slide.attr('contentEditable', false);
+             slide.off('keydown', disableHotkeys);
+           }
+         });
+  });
 });
